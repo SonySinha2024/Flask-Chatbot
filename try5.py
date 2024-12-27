8a53cbf0af02e449be5eb6805e95933da1c628e7
Sony Sinha <107948215+SonySinha2024@users.noreply.github.com>
2024-12-27 14:13:38 +0530

    Update try5.py

--- try5.py
+++ try5.py
@@ -20,7 +20,7 @@ log = log.getLogger(__name__)
 config_file_path = 'C:/frontend/Flask-Chatbot/config/.env'
 load_dotenv(dotenv_path=config_file_path)
 
-google_api_key = "AIzaSyDfVIOcSaPeaRPbAA_pMbHJe9YZAySy-Ig"
+google_api_key = "A"
 if not google_api_key:
     log.error("Required API keys are missing from environment variables.")
     raise EnvironmentError("Required API keys are missing from environment variables.")
